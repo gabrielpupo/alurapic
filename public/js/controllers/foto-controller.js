0e70fd1fa5107cb154efd955b79dd5b34654b743
gabrielpupomoraes <gabmezenga@hotmail.com>
2018-02-07 23:59:28 -0200

    aula 8 p1

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,10 +1,24 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http) {
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams) {
 
     $scope.foto = {};
     $scope.mensagem = '';
 
-    $scope.submeter = function() {
+    console.log($routeParams.fotoid);
+    
 
+    if($routeParams.fotoid){
+        $http.get('v1/fotos/' + $routeParams.fotoid)
+        .success(function(foto) {
+            $scope.foto = foto;
+        })
+        .error(function(erro) {
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível obter a foto de id ' + $routeParams.fotoId;
+        });
+    }
+    
+
+    $scope.submeter = function() {
         if ($scope.formulario.$valid) {
             $http.post('v1/fotos', $scope.foto)
             .success(function(){
@@ -14,10 +28,8 @@ angular.module('alurapic').controller('FotoController', function($scope, $http) 
             .error(function(erro){
                 $scope.mensagem = 'Não Foi Possível Incluir a Foto';
                 console.log(erro);
-                
             });
         }
-   
     };
 
 });
--- public/js/main.js
+++ public/js/main.js
@@ -13,6 +13,11 @@ angular.module('alurapic', ['minhasDiretivas','ngAnimate','ngRoute'])
         controller: 'FotoController'
     });
 
+    $routeProvider.when('/fotos/edit/:fotoid', {
+        templateUrl: 'partials/foto.html',
+        controller: 'FotoController'
+    });    
+
     $routeProvider.otherwise({redirectTo: '/fotos'});
 
 });
