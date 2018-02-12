c0979602ae5a9b5a84ab4e06e3e2b9a633d616b7
gabrielpupomoraes <gabmezenga@hotmail.com>
2018-02-12 09:33:53 -0200

    aula 9

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,35 +1,33 @@
 angular.module('minhasDiretivas', [])
 .directive('meuPainel', function(){
-
     var ddo = {};
-
     ddo.restrict = "AE";
-
     ddo.transclude = true;
-
     ddo.scope = {
         titulo: '@'
     };
-
     ddo.templateUrl = 'js/directives/meu-painel.html' ;
-
     return ddo;
-
-}).directive('minhaFoto', function(){
-
+})
+.directive('minhaFoto', function(){
     var ddo = {};
-
     ddo.restrict = "AE";
-
     ddo.transclude = true;
-
     ddo.scope = {
         titulo: '@',
         url: '@' 
     };
-
     ddo.templateUrl = 'js/directives/minha-foto.html' ;
-
     return ddo;
-
-});
+})
+.directive('meuBotaoPerigo', function(){
+    var ddo = {};
+    ddo.restrict = "AE";
+    ddo.transclude = true;
+    ddo.scope = {
+        nome: '@',
+        acao: '&' 
+    };
+    ddo.template = '<button ng-click="acao()" class="btn btn-danger btn-block">{{nome}}</button>';
+    return ddo;
+})
